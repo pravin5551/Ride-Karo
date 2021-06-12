fbe33e6e127e39007305f8c8ac941761d8828daf
pravin5551 <pravinvpawar5551@gmail.com>
2021-06-12 10:50:14 +0530

    UI and firebase

---
+++ app/src/main/java/com/froyo/ridekaro/views/OTPSecondActivity.kt
@@ -0,0 +1,12 @@
+package com.froyo.ridekaro.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.froyo.ridekaro.R
+
+class OTPSecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otpsecond)
+    }
+}
