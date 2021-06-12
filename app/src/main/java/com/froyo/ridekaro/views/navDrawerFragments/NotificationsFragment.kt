a977f7b72ae971b48cb3c5db84b5415f2407ab13
harsh <harshraga7@gmail.com>
2021-06-13 00:39:44 +0530

    Notification and powerpass

--- app/src/main/java/com/froyo/ridekaro/views/navDrawerFragments/NotificationsFragment.kt
+++ app/src/main/java/com/froyo/ridekaro/views/navDrawerFragments/NotificationsFragment.kt
@@ -1,11 +1,14 @@
 package com.froyo.ridekaro.views.navDrawerFragments
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.froyo.ridekaro.R
+
 
 class NotificationsFragment : Fragment() {
 
@@ -13,8 +16,17 @@ class NotificationsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false)
+
+        // create ContextThemeWrapper from the original Activity Context with the custom theme
+        val contextThemeWrapper: Context = ContextThemeWrapper(activity, R.style.Theme_Notification)
+
+
+        // clone the inflater using the ContextThemeWrapper
+        val localInflater = inflater.cloneInContext(contextThemeWrapper)
+
+
+        // inflate the layout using the cloned inflater, not default inflater
+        return localInflater.inflate(R.layout.fragment_notifications, container, false)
     }
 
 
