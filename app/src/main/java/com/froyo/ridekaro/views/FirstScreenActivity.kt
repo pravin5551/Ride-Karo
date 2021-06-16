06c07c39bc676789f4d3b9b8c2f6aacbf289e7f3
harsh <harshraga7@gmail.com>
2021-06-16 14:45:34 +0530

    made changes

--- app/src/main/java/com/froyo/ridekaro/views/FirstScreenActivity.kt
+++ app/src/main/java/com/froyo/ridekaro/views/FirstScreenActivity.kt
@@ -19,8 +19,8 @@ import kotlinx.android.synthetic.main.activity_first_screen.*
 class FirstScreenActivity : AppCompatActivity() {
 
     val FINE_LOCATION_RO = 101
-    val sharePref=getSharedPreferences("myPre", Context.MODE_PRIVATE)
-    val editor=sharePref.edit()
+//    val sharePref=getSharedPreferences("myPre", Context.MODE_PRIVATE)
+//    val editor=sharePref.edit()
 
 
 
