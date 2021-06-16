8ea2ba8b25b492a339c6f5b2857c5dbeecdc9d2b
pravin5551 <pravinvpawar5551@gmail.com>
2021-06-17 01:23:00 +0530

    Book Vaccination Added

---
+++ app/src/main/java/com/froyo/ridekaro/views/Book_Vaccine_Activity.kt
@@ -0,0 +1,51 @@
+package com.froyo.ridekaro.views
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.froyo.ridekaro.R
+import kotlinx.android.synthetic.main.activity_book_vaccine.*
+
+class Book_Vaccine_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_vaccine)
+
+
+
+        if (WebViewwallstreetjournal != null) {
+            val webSettings = WebViewwallstreetjournal!!.settings
+            webSettings.javaScriptEnabled = true
+            WebViewwallstreetjournal!!.webViewClient = WebViewClient()
+            WebViewwallstreetjournal!!.loadUrl("https://selfregistration.cowin.gov.in/")
+
+    //https://www.wsj.com/
+            WebViewwallstreetjournal!!.webViewClient = object : WebViewClient() {
+
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+
+                    wsjProgressBar.visibility = View.VISIBLE
+                    super.onPageStarted(view, url, favicon)
+
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    wsjProgressBar.visibility = View.GONE
+                    super.onPageFinished(view, url)
+
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (WebViewwallstreetjournal!!.canGoBack()) {
+            WebViewwallstreetjournal!!.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
