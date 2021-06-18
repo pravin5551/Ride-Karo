c0a9501bf64313f1ea9fc654da5150f76a7937e7
Chaithanya Gollapalli <chaitanyagcg.01@gmail.com>
2021-06-18 13:09:54 +0530

    for pulling changes

--- app/src/main/java/com/froyo/ridekaro/views/OTPValidation.kt
+++ app/src/main/java/com/froyo/ridekaro/views/OTPValidation.kt
@@ -1,15 +1,13 @@
 package com.froyo.ridekaro.views
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.froyo.ridekaro.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_otpvalidation.*
@@ -31,34 +29,34 @@ class OTPValidation : AppCompatActivity() {
             .build()
 
 
-//        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        btnGoogle.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent;
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+
+            val acct = GoogleSignIn.getLastSignedInAccount(applicationContext)
+            if (acct != null) {
 //
-//        btnGoogle.setOnClickListener {
-//            val signInIntent = mGoogleSignInClient.signInIntent;
-//            startActivityForResult(signInIntent, RC_SIGN_IN);
+//                val bundle = Bundle()
+//                val name = acct.displayName
+//                val email = acct.email
 //
-//            val acct = GoogleSignIn.getLastSignedInAccount(applicationContext)
-//            if (acct != null) {
-////
-////                val bundle = Bundle()
-////                val name = acct.displayName
-////                val email = acct.email
-////
-////                val bundle = bundleOf("name" to acct.displayName, "email" to acct.email,"photo" to acct.photoUrl.toString())
-////
-////                val fragment = ProfileFragment()
-////                val fragmentManager: FragmentManager = supportFragmentManager
-////                val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-////                fragment.arguments = (bundle)
-////                fragmentTransaction.replace(R.id.googleContainer, fragment,"fragment").addToBackStack("fragment").commit()
-//                val intent_two = Intent(this, HomeActivity::class.java)
-//                intent_two.putExtra("UserName",acct.displayName)
-//                intent_two.putExtra("UserEmail",acct.email)
-//                intent_two.putExtra("UserPhoto",acct.photoUrl.toString())
-//                startActivity(intent_two)
-//            }
+//                val bundle = bundleOf("name" to acct.displayName, "email" to acct.email,"photo" to acct.photoUrl.toString())
 //
-//        }
+//                val fragment = ProfileFragment()
+//                val fragmentManager: FragmentManager = supportFragmentManager
+//                val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+//                fragment.arguments = (bundle)
+//                fragmentTransaction.replace(R.id.googleContainer, fragment,"fragment").addToBackStack("fragment").commit()
+                val intent_two = Intent(this, HomeActivity::class.java)
+                intent_two.putExtra("UserName",acct.displayName)
+                intent_two.putExtra("UserEmail",acct.email)
+                intent_two.putExtra("UserPhoto",acct.photoUrl.toString())
+                startActivity(intent_two)
+            }
+
+        }
 
         button.setOnClickListener {
 
@@ -72,6 +70,14 @@ class OTPValidation : AppCompatActivity() {
             }
         }
     }
+
+//    override fun onStart() {
+//        super.onStart()
+//        // Check for existing Google Sign In account, if the user is already signed in
+//// the GoogleSignInAccount will be non-null.
+//        val account = GoogleSignIn.getLastSignedInAccount(this)
+//        updateUI(account)
+//    }
 
 //    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 //        super.onActivityResult(requestCode, resultCode, data)
@@ -139,17 +145,17 @@ class OTPValidation : AppCompatActivity() {
 //    }
 
 
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//
-//        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-//        if (requestCode == RC_SIGN_IN) {
-//            // The Task returned from this call is always completed, no need to attach
-//            // a listener.
-//            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-//            handleSignInResult(task)
-//        }
-//    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
 
